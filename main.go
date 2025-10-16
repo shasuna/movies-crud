648ae7017855c112a10d1a9687c4cce35dc2102d
jamie <hobbiathan@gmail.com>
2025-10-15 23:16:10 -0600

    PUT /movies/{id}

--- main.go
+++ main.go
@@ -65,6 +65,25 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+// This update function deletes and recreates a movie entry - obviously not what you'd want in prod db
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// Delete entry
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(10000000))
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
